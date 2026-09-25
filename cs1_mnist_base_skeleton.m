616ea67b29d260836dd2181defe23017cb4494ba
LQ458 <lqn458@gmail.com>
2026-09-25 14:20:19 -0500

    Finalize skeleton scripts with k=400 classifier; remove analysis files

--- cs1_mnist_base_skeleton.m
+++ cs1_mnist_base_skeleton.m
@@ -1,89 +1,241 @@
-clear;
+
+clear all;
 close all;
 
-k = 100; % Change k here to compare different numbers of clusters.
-max_iter = 50;
-rng(1050); % Repeatable random initialization.
+%% In this script, you need to implement three functions as part of the k-means algorithm.
+% These steps will be repeated until the algorithm converges:
 
-%% Read the data
-train = readmatrix('mnist_train_1500.csv');
+  % 1. initialize_centroids
+  % This function sets the initial values of the centroids
+  
+  % 2. assign_vector_to_centroid
+  % This goes through the collection of all vectors and assigns them to
+  % centroid based on norm/distance
+  
+  % 3. update_centroids
+  % This function updates the location of the centroids based on the collection
+  % of vectors (handwritten digits) that have been assigned to that centroid.
+
+
+%% Initialize Data Set
+% These next lines of code read in two sets of MNIST digits that will be used for training and testing respectively.
+
+% training set (1500 images)
+train=csvread('mnist_train_1500.csv');
 trainsetlabels = train(:,785);
-train(:,785) = 0;
-test = readmatrix('mnist_test_200.csv');
+train=train(:,1:784);
+train(:,785)=zeros(1500,1);
+
+% testing set (200 images with 11 outliers)
+test=csvread('mnist_test_200.csv');
+% store the correct test labels
 correctlabels = test(:,785);
-test(:,785) = 0;
+test=test(:,1:784);
 
-%% Assign images, update centroids, and record cost
-centroids = initialize_centroids(train,k);
-cost_iteration = zeros(max_iter,1);
-for iter = 1:max_iter
-    old_assignments = train(:,785);
-    for i = 1:size(train,1)
-        train(i,785) = assign_vector_to_centroid(train(i,:),centroids);
-    end
-    centroids = update_Centroids(train,k,centroids);
-    residuals = train(:,1:784) - centroids(train(:,785),1:784);
-    cost_iteration(iter) = sum(residuals(:).^2);
-    if isequal(train(:,785),old_assignments)
-        break;
-    end
-end
-cost_iteration = cost_iteration(1:iter);
+% now, zero out the labels in "test" so that you can use this to assign
+% your own predictions and evaluate against "correctlabels"
+% in the 'cs1_mnist_evaluate_test_set.m' script
+test(:,785)=zeros(200,1);
 
-%% Assign each centroid the most common training label in its cluster
-centroid_labels = zeros(k,1);
-for j = 1:k
-    members = train(:,785)==j;
-    if any(members)
-        centroid_labels(j) = mode(trainsetlabels(members));
-    else
-        % If a cluster is empty, use the nearest training image's label.
-        d = sum((train(:,1:784)-centroids(j,1:784)).^2,2);
-        [~,nearest] = min(d);
-        centroid_labels(j) = trainsetlabels(nearest);
-    end
-end
+%% After initializing, you will have the following variables in your workspace:
+% 1. train (a 1500 x 785 array, containins the 1500 training images)
+% 2. test (a 200 x 785 array, containing the 200 testing images)
+% 3. correctlabels (a 200 x 1 array containing the correct labels (numerical
+% meaning) of the 200 test images
+
+%% To visualize an image, you need to reshape it from a 784 dimensional array into a 28 x 28 array.
+% to do this, you need to use the reshape command, along with the transpose
+% operation.  For example, the following lines plot the first test image
+
+figure;
+colormap('gray'); % this tells MATLAB to depict the image in grayscale
+testimage = reshape(test(1,[1:784]), [28 28]);
+% we are reshaping the first row of 'test', columns 1-784 (since the 785th
+% column is going to be used for storing the centroid assignment.
+imagesc(testimage'); % this command plots an array as an image.  Type 'help imagesc' to learn more.
+
+%% After importing, the array 'train' consists of 1500 rows and 785 columns.
+% Each row corresponds to a different handwritten digit (28 x 28 = 784)
+% plus the last column, which is used to index that row (i.e., label which
+% cluster it belongs to.  Initially, this last column is set to all zeros,
+% since there are no clusters yet established.
+
+%% This next section of code calls the three functions you are asked to specify
+
+k=400; % set k
+max_iter=50; % set the number of iterations of the algorithm
+
+rng(1050);
+[centroids,train,cost_iteration]=run_kmeans(train,k,max_iter);
+
+centroid_labels=label_centroids(train,trainsetlabels,k);
 save('classifierdata.mat','centroids','centroid_labels');
 
-%% Figure 1: k-means cost
-figure('Color','w');
-if isprop(gcf,'Theme'), set(gcf,'Theme','light'); end
-plot(1:iter,cost_iteration,'-o','LineWidth',1.5);
-xlabel('Iteration'); ylabel('K-means cost (WCSS)');
-title(sprintf('Training cost, k = %d',k)); grid on;
+%% This section of code plots the k-means cost as a function of the number
+% of iterations
 
-%% Figure 2: centroids and their digit labels
-figure('Color','w');
-if isprop(gcf,'Theme'), set(gcf,'Theme','light'); end
+figure;
+plot(cost_iteration,'-o','LineWidth',2);
+xlabel('Iteration');
+ylabel('K-means cost');
+title('K-means cost vs. iteration');
+grid on;
+set(gca,'FontSize',12);
+
+
+%% This next section of code will make a plot of all of the centroids
+% Again, use help <functionname> to learn about the different functions
+% that are being used here.
+
+figure;
+colormap('gray');
+
 plotsize = ceil(sqrt(k));
-for j = 1:k
-    subplot(plotsize,plotsize,j);
-    imagesc(reshape(centroids(j,1:784),[28 28])',[0 255]);
+[~,order]=sort(centroid_labels);
+
+for ind=1:k
+    
+    centr=centroids(order(ind),[1:784]);
+    subplot(plotsize,plotsize,ind);
+    
+    imagesc(reshape(centr,[28 28])');
     axis image off;
-    title(sprintf('%d: %d',j,centroid_labels(j)));
-end
-colormap gray;
 
-function y = initialize_centroids(data,num_centroids)
-    random_index = randperm(size(data,1));
-    y = data(random_index(1:num_centroids),:);
 end
+sgtitle('Centroids sorted by digit label');
 
-function [index,vec_distance] = assign_vector_to_centroid(data,centroids)
-    d = zeros(size(centroids,1),1);
-    for j = 1:size(centroids,1)
-        d(j) = norm(data(1:784)-centroids(j,1:784));
+k_values=[10 20 50 100 200 300 400 500 1000];
+train_accuracy=zeros(length(k_values),1);
+test_accuracy=zeros(length(k_values),1);
+final_cost=zeros(length(k_values),1);
+digits_with_centroid=zeros(length(k_values),1);
+
+for n=1:length(k_values)
+    rng(1050);
+    [k_centroids,k_train,k_cost]=run_kmeans(train,k_values(n),max_iter);
+    k_labels=label_centroids(k_train,trainsetlabels,k_values(n));
+    train_accuracy(n)=mean(k_labels(k_train(:,785))==trainsetlabels);
+    k_predictions=zeros(size(test,1),1);
+    for i=1:size(test,1)
+        k_predictions(i)=k_labels(assign_vector_to_centroid(test(i,:),k_centroids));
     end
-    [vec_distance,index] = min(d);
+    test_accuracy(n)=mean(k_predictions==correctlabels);
+    final_cost(n)=k_cost(end);
+    digits_with_centroid(n)=length(unique(k_labels));
 end
 
-function new_centroids = update_Centroids(data,K,previous_centroids)
-    % Retain the previous position when a cluster has no members.
-    new_centroids = previous_centroids;
-    for j = 1:K
-        members = data(:,785)==j;
-        if any(members)
-            new_centroids(j,1:784) = mean(data(members,1:784),1);
-        end
+k_results=table(k_values',100*train_accuracy,100*test_accuracy,final_cost,digits_with_centroid, ...
+    'VariableNames',{'k','train_accuracy','test_accuracy','cost','digits_with_centroid'})
+
+digit_average=zeros(10,784);
+own_average_distance=zeros(10,1);
+for d=0:9
+    members=train(trainsetlabels==d,1:784);
+    digit_average(d+1,:)=mean(members,1);
+    for i=1:size(members,1)
+        own_average_distance(d+1)=own_average_distance(d+1)+norm(members(i,:)-digit_average(d+1,:))/size(members,1);
     end
+end
+
+average_distance=zeros(10,10);
+for a=1:10
+    for b=1:10
+        average_distance(a,b)=norm(digit_average(a,:)-digit_average(b,:));
+    end
+end
+
+digit_names=string(0:9);
+average_distance_table=array2table(round([average_distance own_average_distance]), ...
+    'RowNames',digit_names,'VariableNames',[digit_names "own_digit"])
+
+function [centroids,data,cost_iteration]=run_kmeans(data,k,max_iter)
+
+data(:,785)=zeros(size(data,1),1);
+
+%% The next line initializes the centroids.  Look at the initialize_centroids()
+% function, which is specified further down this file.
+
+centroids=initialize_centroids(data,k);
+
+%% Initialize an array that will store k-means cost at each iteration
+
+cost_iteration = zeros(max_iter, 1);
+
+%% This for-loop enacts the k-means algorithm
+
+for iter=1:max_iter
+    
+    previous_assignment=data(:,785);
+    for i=1:size(data,1)
+        [index, vec_distance]=assign_vector_to_centroid(data(i,:),centroids);
+        data(i,785)=index;
+        cost_iteration(iter)=cost_iteration(iter)+vec_distance^2;
+    end
+    centroids=update_Centroids(data,k,centroids);
+    if isequal(data(:,785),previous_assignment)
+        break;
+    end
+    
+end
+cost_iteration=cost_iteration(1:iter);
+
+end
+
+%% Function to initialize the centroids
+% This function randomly chooses k vectors from our training set and uses them to be our initial centroids
+% There are other ways you might initialize centroids.
+% ***Feel free to experiment.***
+% Note that this function takes two inputs and emits one output (y).
+
+function y=initialize_centroids(data,num_centroids)
+
+random_index=randperm(size(data,1));
+
+centroids=data(random_index(1:num_centroids),:);
+
+y=centroids;
+
+end
+
+%% Function to pick the Closest Centroid using norm/distance
+% This function takes two arguments, a vector and a set of centroids
+% It returns the index of the assigned centroid and the distance between
+% the vector and the assigned centroid.
+
+function [index, vec_distance] = assign_vector_to_centroid(data,centroids)
+
+distances=zeros(size(centroids,1),1);
+for j=1:size(centroids,1)
+    distances(j)=norm(data(1:784)-centroids(j,1:784));
+end
+[vec_distance,index]=min(distances);
+
+end
+
+
+%% Function to compute new centroids using the mean of the vectors currently assigned to the centroid.
+% This function takes the set of training images, the value of k, and the
+% previous centroids.
+% It returns a new set of centroids based on the current assignment of the
+% training images.
+
+function new_centroids=update_Centroids(data,K,previous_centroids)
+
+new_centroids=previous_centroids;
+for j=1:K
+    members=data(:,785)==j;
+    if any(members)
+        new_centroids(j,1:784)=mean(data(members,1:784),1);
+    end
+end
+
+end
+
+function labels=label_centroids(data,trainsetlabels,K)
+
+labels=zeros(K,1);
+for j=1:K
+    labels(j)=mode(trainsetlabels(data(:,785)==j));
+end
+
 end
--- cs1_mnist_evaluate_test_set.m
+++ cs1_mnist_evaluate_test_set.m
@@ -1,49 +1,113 @@
-if ~exist('test','var')
-    test = readmatrix('mnist_test_200.csv');
-    correctlabels = test(:,785);
-    test(:,785) = 0;
-end
+%% This code evaluates the test set.
 
-%% Predict using only the pixel columns
-n_test = size(test,1);
+% ** Important.  This script requires that:
+% 1)'centroid_labels' be established in the workspace
+% AND
+% 2)'centroids' be established in the workspace
+% AND
+% 3)'test' be established in the workspace
+
+
+% IMPORTANT!!:
+% You should save 1) and 2) in a file named 'classifierdata.mat' as part of
+% your submission.
+
+load('classifierdata.mat');
+
+n_test=size(test,1);
 predictions = zeros(n_test,1);
+outliers = zeros(n_test,1);
+distances = zeros(n_test,1);
+
+% loop through the test set, figure out the predicted number
 for i = 1:n_test
-    prediction_index = assign_vector_to_centroid(test(i,:),centroids);
-    predictions(i) = centroid_labels(prediction_index);
+
+testing_vector=test(i,:);
+
+% Extract the centroid that is closest to the test image
+[prediction_index, vec_distance]=assign_vector_to_centroid(testing_vector,centroids);
+
+predictions(i) = centroid_labels(prediction_index);
+distances(i) = vec_distance;
+
 end
 
-%% Flag pixels outside the stated grayscale range
-% Flagged images are still classified and included in the overall accuracy.
-outliers = double(any(test(:,1:784)<0 | test(:,1:784)>255,2));
-figure('Color','w');
-if isprop(gcf,'Theme'), set(gcf,'Theme','light'); end
-stem(1:n_test,outliers,'filled');
-xlabel('Test image index'); ylabel('Outlier flag');
-title('Pixels outside [0,255]'); ylim([-0.1 1.2]); grid on;
-
-%% Figure 4: true and predicted labels
-figure('Color','w');
-if isprop(gcf,'Theme'), set(gcf,'Theme','light'); end
-if exist('correctlabels','var') && numel(correctlabels)==n_test
-    correctlabels = correctlabels(:);
-    correct_count = sum(predictions==correctlabels);
-    accuracy = correct_count/n_test;
-    fprintf('Correct: %d/%d; accuracy: %.2f%%\n', ...
-        correct_count,n_test,100*accuracy);
-    plot(1:n_test,correctlabels,'o'); hold on;
-    plot(1:n_test,predictions,'x'); hold off;
-    legend('True label','Prediction','Location','best');
-    title(sprintf('Predictions: %d/%d correct',correct_count,n_test));
-else
-    plot(1:n_test,predictions,'x');
-    title('Predicted digit labels');
-end
-xlabel('Test image index'); ylabel('Digit'); yticks(0:9); grid on;
-
-function [index,vec_distance] = assign_vector_to_centroid(data,centroids)
-    d = zeros(size(centroids,1),1);
-    for j = 1:size(centroids,1)
-        d(j) = norm(data(1:784)-centroids(j,1:784));
+%% DESIGN AND IMPLEMENT A STRATEGY TO SET THE outliers VECTOR
+% outliers(i) should be set to 1 if the i^th entry is an outlier
+% otherwise, outliers(i) should be 0
+for i = 1:n_test
+    if min(test(i,1:784))<0 || max(test(i,1:784))>255
+        outliers(i)=1;
     end
-    [vec_distance,index] = min(d);
+end
+
+%% MAKE A STEM PLOT OF THE OUTLIER FLAG
+figure;
+stem(outliers,'filled','LineWidth',1.5);
+xlabel('Test image');
+ylabel('Outlier flag');
+title('Test images with pixels outside 0 to 255');
+ylim([0 1.2]);
+grid on;
+set(gca,'FontSize',12);
+
+%% The following plots the correct and incorrect predictions
+% Make sure you understand how this plot is constructed
+figure;
+plot(correctlabels,'o','LineWidth',1.5);
+hold on;
+plot(predictions,'x','LineWidth',1.5);
+title('Predictions');
+xlabel('Test image');
+ylabel('Digit');
+yticks(0:9);
+legend('Correct label','Prediction','Location','northoutside','Orientation','horizontal');
+grid on;
+set(gca,'FontSize',12);
+
+%% The following line provides the number of instances where and entry in correctlabel is
+% equatl to the corresponding entry in prediction
+% However, remember that some of these are outliers
+sum(correctlabels==predictions)
+
+accuracy=sum(correctlabels==predictions)/n_test
+
+outlier_count=sum(outliers)
+outlier_correct=sum(correctlabels(outliers==1)==predictions(outliers==1))
+median_distance_outliers=median(distances(outliers==1))
+median_distance_others=median(distances(outliers==0))
+[~,farthest]=sort(distances,'descend');
+outliers_among_farthest=sum(outliers(farthest(1:outlier_count)))
+
+confusion=zeros(10,10);
+for i = 1:n_test
+    confusion(correctlabels(i)+1,predictions(i)+1)=confusion(correctlabels(i)+1,predictions(i)+1)+1;
+end
+confusion_table=array2table(confusion,'RowNames',"true_"+string(0:9),'VariableNames',"predicted_"+string(0:9))
+
+figure;
+colormap('gray');
+outlier_index=find(outliers==1);
+wrong_index=find(predictions~=correctlabels & outliers==0);
+for ind=1:min(4,length(outlier_index))
+    subplot(2,4,ind);
+    imagesc(reshape(test(outlier_index(ind),1:784),[28 28])',[0 255]);
+    axis image off;
+    title([num2str(correctlabels(outlier_index(ind))) ' as ' num2str(predictions(outlier_index(ind)))]);
+end
+for ind=1:min(4,length(wrong_index))
+    subplot(2,4,ind+4);
+    imagesc(reshape(test(wrong_index(ind),1:784),[28 28])',[0 255]);
+    axis image off;
+    title([num2str(correctlabels(wrong_index(ind))) ' as ' num2str(predictions(wrong_index(ind)))]);
+end
+
+function [index, vec_distance] = assign_vector_to_centroid(data,centroids)
+
+distances=zeros(size(centroids,1),1);
+for j=1:size(centroids,1)
+    distances(j)=norm(data(1:784)-centroids(j,1:784));
+end
+[vec_distance,index]=min(distances);
+
 end
